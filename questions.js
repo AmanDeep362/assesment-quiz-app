788d7784d9b6de8c2879b84831727eaf48e97b6f
AmanDeep362 <84900204+AmanDeep362@users.noreply.github.com>
2021-11-12 13:35:53 +0530

    commit changes

--- questions.js
+++ questions.js
@@ -5,6 +5,7 @@ let questions = [
       "The Binary Coded Decimal (BCD) uses",
     options: ["6 bits", "8 bits", "16 bits", "32 bits"],
     correctOption: "6 bits",
+    checkedoption:[],
   },
   {
     QueNo: 2,
@@ -12,12 +13,14 @@ let questions = [
       "A program attempts to generate as many permutations as possible of the string, 'abcd' by pushing the characters a, b, c, d in the same order onto a stack, but it may pop off the top character at any time. Which one of the following strings CANNOT be generated using this program?",
     options: ["abcd", "dcba", "cbad", "cabd"],
     correctOption: "cbad",
+    checkedoption:[]
   },
   {
     QueNo: 3,
     question: "RAM is also called as",
     options: ["virtual memory", "volatile memory", "Non volatile memory", "cache memory"],
-    correctOption: "virtual memory"
+    correctOption: "virtual memory",
+    checkedoption:[]
   },
   {
     QueNo: 4,
@@ -25,6 +28,7 @@ let questions = [
       "The Binary Coded Decimal (BCD) uses",
     options: ["6 bits", "8 bits", "16 bits", "32 bits"],
     correctOption: "6 bits",
+    checkedoption:[]
   },
   {
     QueNo: 5,
@@ -32,12 +36,14 @@ let questions = [
       "A program attempts to generate as many permutations as possible of the string, 'abcd' by pushing the characters a, b, c, d in the same order onto a stack, but it may pop off the top character at any time. Which one of the following strings CANNOT be generated using this program?",
     options: ["abcd", "dcba", "cbad", "cabd"],
     correctOption: "cbad",
+    checkedoption:[]
   },
   {
     QueNo: 6,
     question: "RAM is also called as",
     options: ["virtual memory", "volatile memory", "Non volatile memory", "cache memory"],
-    correctOption: "virtual memory"
+    correctOption: "virtual memory",
+    checkedoption:[]
   },
   {
     QueNo: 7,
@@ -45,15 +51,17 @@ let questions = [
       "A program attempts to generate as many permutations as possible of the string, 'abcd' by pushing the characters a, b, c, d in the same order onto a stack, but it may pop off the top character at any time. Which one of the following strings CANNOT be generated using this program?",
     options: ["abcd", "dcba", "cbad", "cabd"],
     correctOption: "cbad",
+    checkedoption:[]
   },
   
 ];
 
 let i = 0;
+
 let noofquest = questions.length;
 console.log(noofquest);
 function questionview() {
-  document.getElementById("questionNo").innerText = questions[i].QueNo;
+  document.getElementById("question-No").innerText = questions[i].QueNo;
   document.getElementById("questionno").innerText = questions[i].QueNo;
   document.getElementById("totalQues").innerText = noofquest;
   document.getElementById("question").innerText = questions[i].question;
@@ -78,23 +86,46 @@ function questionview() {
     <span class="option-text" id="option-4-text">${questions[i].options[3]}</span>
   </label>
   `
-  if (i == 0) {
-    document.getElementById("btn-prev").disabled = true;
-  } else {
-    document.getElementById("btn-prev").disabled = false;
-  }
-  if (i == noofquest - 1) {
+
+
+  document.getElementById("question-container")
+  // if (i == 0) {
+  //   document.getElementById("btn-prev").disabled = true;
+  // } else {
+  //   document.getElementById("btn-prev").disabled = false;
+  // }
+  if (i == noofquest ) {
     document.getElementById("btn-next").disabled = true;
   } else {
     document.getElementById("btn-next").disabled = false;
   }
+  
+// if(i<noofquest){
+//   document.getElementById("submit-btn").disabled = true;
+// }
+// else if(i==noofquest-1){
+//   document.getElementById("submit-btn").disabled = false;
+// }
+  
 }
 questionview();
-document.getElementById("btn-prev").onclick = () => {
-  i = i - 1;
-  questionview();
-};
+
+
+
+// document.getElementById("btn-prev").onclick = () => {
+//   i = i - 1;
+//   questionview();
+// };
 document.getElementById("btn-next").onclick = () => {
+  var radios = document.getElementsByName("option");
+  for (var k = 0; k < radios.length; k++) {
+    if (radios[k].checked) {
+       questions[i].checkedoption.push(radios[k].id)
+       break;
+     }
+     
+  }
+  console.log(questions)
   i = i + 1;
   questionview();
 };
